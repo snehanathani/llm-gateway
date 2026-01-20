27ebb8c9d9ce1efea28a39a02cf741c27a3c9968
sanaa <snehanathani@gmail.com>
2026-01-20 23:21:28 +0530

    initializer setup

---
+++ src/main/java/com/example/llm_gateway/LlmGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.llm_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmGatewayApplication.class, args);
+	}
+
+}
